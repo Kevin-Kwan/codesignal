e9de4ae9101e27579038fab95586a5657e161edc
Kevin <kkwan9@gatech.edu>
2022-08-17 23:38:04 -0400

    Create adjacentElementsProduct.java

---
+++ Intro/The-Journey-Begins/adjacentElementsProduct.java
@@ -0,0 +1,10 @@
+int solution(int[] inputArray) {
+    int max = inputArray[0]*inputArray[1];
+    for (int i = 1; i<inputArray.length-1;i++) {
+        if (inputArray[i]*inputArray[i+1]>max) {
+            max = inputArray[i]*inputArray[i+1];
+        }
+    }
+        
+    return max;
+}
